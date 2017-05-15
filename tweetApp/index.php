9e960290e59aa6fd3743723aa1f1a00fdd08a0f9
Joanna Trojak <joannatrojak19@gmail.com>
2017-05-15 14:13:30 +0200

    newuser form

--- tweetApp/index.php
+++ tweetApp/index.php
@@ -6,11 +6,13 @@
     require_once 'head.php';
 
     require_once "User.php";
-
+    
     $newUser = new User();
     $newUser->setUsername('newUser');
     $newUser->saveToDB($mysqli);
 
     echo 'Witaj na stronie głównej';
     require_once 'foot.php';
+    
+    require_once "newuser.php"; 
 ?>
---
+++ tweetApp/newuser.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <form>
+            <input type="text" name="username" placeholder="Username:">
+            <br>
+            <br>
+            <input type="email" name="email" placeholder="email">
+            <br>
+            <br>
+            <input type="password" name="password" placeholder="password">
+            <br>
+            <br>
+            <input type="submit" value="Utwórz nowego użytkownika">
+        </form>
+    </body>
+</html>
