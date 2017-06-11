43d6438b44c194fc130e7d84776c48f35f149985
Joanna Trojak <joannatrojak19@gmail.com>
2017-06-11 17:50:25 +0200

    login pageload from db

--- tweetApp/login.php
+++ tweetApp/login.php
@@ -4,6 +4,25 @@ if (isset($_POST['username']) && isset($_POST['password']))
     $username = $_POST['username']; 
     $password = $_POST['password']; 
     
+    $sql = "SELECT * FROM Users"; 
+    $result = $mysqli->query($sql); 
+    
+    if ($result == true && $result->num_rows>0)
+    {
+        foreach ($result as $row)
+        {
+            if ($row['username'] && password_verify($password, $row['hashed_password']))
+            {
+                $id = $row['id']; 
+                
+                if ($id>0)
+                {
+                    $_SESSION['userId'] = $id; 
+                    $_SESSION['username'] = $username; 
+                }
+            }
+        }
+    }
 }
 ?>
 <html>
