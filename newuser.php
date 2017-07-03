3090e2dc481d5fe2d08b14d1c8dbee1c48c3a8f2
JTrojak19 <joannatrojak19@gmail.com>
2017-07-03 11:06:07 +0200

    Modify newuser

--- newuser.php
+++ newuser.php
@@ -1,16 +1,56 @@
-<html>
-    <body>
-        <form action="index.php" method="post">
-            <input type="text" name="username" placeholder="username">
-            <br>
-            <br>
-            <input type="email" name="email" placeholder="email">
-            <br>
-            <br>
-            <input type="password" name="password" placeholder="password">
-            <br>
-            <br>
-            <input type="submit" value="Register">
-        </form>
-    </body>
-</html>
+<?php
+require_once "User.php";
+
+echo '<form action="" method="post">
+<input type="email" name="email"><br>
+<input type="text" name="username"<br>
+<input type="password" name="pass"<br>
+<input type="password" name="pass2"<br>
+<input type="submit value="Register">
+</form>';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  if (!empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['pass']) !empty($_POST['pass2']))
+  {
+    $email = $_POST['email'];
+    $name = $_POST['username'];
+    $pass = $_POST['pass'];
+    $pass2 = $_POST['pass2'];
+
+    if ($pass === $pass2)
+    {
+      $hashedPass = password_hash($pass, PASSWORD_BYCRYP);
+      $newUser = new User();
+      $newUser->setEmail($email);
+      $newUser->setUsername($name);
+      $newUser->setHashPass($hashedPass);
+      $newUser->saveToDB($conn);
+    }
+    else
+    {
+      echo "Passwords are not identical !!<br>";
+    }
+  }
+  else
+  {
+    echo "Please fill in all the data. You've missed the following fields:<br>";
+    if (empty($_POST['email']))
+    {
+      echo "Email<br>";
+    }
+    if (empty($_POST['username']))
+    {
+      echo "Username<br>";
+    }
+    if (empty($_POST['pass']))
+    {
+      echo "Password<br>";
+    }
+    if (empty($_POST['pass2']))
+    {
+      echo "Re-entered password";
+    }
+  }
+}
+?>
